66334a58de48a60fb24b0a381ec55ed100bf5696
Sam Seabourn <sam.seabourn@gmail.com>
2020-06-25 19:38:36 +1000

    tidy up some code, iframes resizing with image

--- portfolio_2.0.0/src/components/Project.js
+++ portfolio_2.0.0/src/components/Project.js
@@ -5,9 +5,6 @@ import Slider from 'react-slick'
 import GithubIcon2 from '../images/icons/icon_github2.svg'
 import LinkIcon from '../images/icons/icon_url.svg'
 
-
-
-
 const Project = ( props ) => {
   const { imageUrls, writeUp, keyWords, githubUrl, linkText, linkUrl, youtubeUrl } = props;
 
@@ -27,7 +24,6 @@ const Project = ( props ) => {
     arrows: false
   };
 
-    
   //Custom hook to listen to resize and update iframe dimensions
   function useWindowSize() {
     const [ size , setSize ] = useState([0, 0]);
@@ -35,18 +31,20 @@ const Project = ( props ) => {
     useLayoutEffect(() => {
       function updateSize() {
         setSize([window.innerWidth, window.innerHeight]);
-        // var imageElement = document.getElementsByClassName('slider-image')[0]
-        // setIframeSize([imageElement.width , imageElement.height])
+        var imageElement = document.getElementsByClassName('slider-image')[0]
+        setIframeSize([ imageElement.width, imageElement.height ])
       }
       window.addEventListener('resize', updateSize);
       updateSize();
       return () => window.removeEventListener('resize', updateSize);
     }, []);
-    return size;
+    return [size, iframeSize]
   }
 
-  const [size ] = useWindowSize()
-
+  let sizes = useWindowSize()
+  let windowWidth = sizes[0][0]
+  let iframeHeight  = sizes[1][1]
+  let iframeWidth = sizes[1][0]
 
   for (let i = 0; i < imageUrls.length; i++) {
     urls.push({ key: i , value: `${imageUrls[i]}` })
@@ -60,29 +58,26 @@ const Project = ( props ) => {
     if (typeof githubUrl !== 'undefined') return <Button linkText="Github" linkUrl={ githubUrl } icon={ GithubIcon2 } />
   }
 
-  // const youtubeDemo = () => {
-  //   if (typeof youtubeUrl !== 'undefined') return(
-  //       <iframe ref={ iFrameElement } 
-  //               width={ iframeWidth }
-  //               height={ iframeHeight }
-  //               className="slider-video" 
-  //               volume="0" 
-  //               src={ `${youtubeUrl}?autoplay=0&controls=0"&modestbranding=1` } 
-  //               >
-  //       </iframe>
-  //   )
-  // }
+  const youtubeDemo = () => {
+    if (typeof youtubeUrl !== 'undefined') return(
+        <iframe ref={ iFrameElement } 
+                width={ iframeWidth }
+                height={ iframeHeight }
+                className="slider-video" 
+                volume="0" 
+                src={ `${youtubeUrl}?autoplay=0&controls=0"&modestbranding=1` } 
+                onLoad={ () => setIframeSize() }
+                >
+        </iframe>
+    )
+  }
 
-  // const setIframeSize = (element) => {
-  //   if (element != null) {
-  //     element.height = imgElement.current.height;
-  //     element.width = imgElement.current.width;
-  //   }
-
-  //   if (imgElement.current.width > 631 ) { // 639 is half minimum screen size
-
-  //   }
-  // }
+  const setIframeSize = (element) => {
+    if (element != null) {
+      element.height = imgElement.current.height;
+      element.width = imgElement.current.width;
+    }
+  }
 
   const guid = () => {  //React warning says that eveything needs a key as it could be breaking in the future
     function _p8(s) {  
@@ -92,30 +87,8 @@ const Project = ( props ) => {
     return _p8() + _p8(true) + _p8(true) + _p8();  
   } 
 
-  // return (
-  //   <div ref={projectContainer} className="project" style={{ background: "rgba(66,72,94, 0.4)" }}>
-  //     <div className="project-slider">
-  //       <Slider {...sliderSettings}>
-  //         { urls.map( ( url ) => { return <img ref={ imgElement } className="slider-image" src={ url.value } key={ guid() }></img> }) }
-  //         { youtubeDemo() }
-  //       </Slider>
-  //     </div>
-  //     <div className="project-text" onLoad={ () => setIframeSize( iFrameElement.current ) }>
-  //       <p style={{ color: "#fff" }}>
-  //         { writeUpText.map( ( word ) => { 
-  //           return keyWords.includes(word)? <strong key={ guid() } >{ `${word} ` }</strong> : `${word} ` }) 
-  //         }
-  //       </p>
-  //       <div className="button-container">
-  //         { urlButton() }
-  //         { githubButton() }
-  //       </div>
-  //     </div>
-  //   </div>
-  // )
-
   const view = () => {
-    if (size >= 1280) {
+    if (windowWidth >= 1280) {
       return (
         //Big Screen
         <div className="project-columns">
@@ -130,40 +103,40 @@ const Project = ( props ) => {
         </div>
         <div className="slider-images">
           <Slider {...sliderSettings}>
-            { urls.map( ( url ) => { return <img ref={ imgElement } src={ url.value } key={ guid() }></img> }) }
+            { urls.map( ( url ) => { return <img ref={ imgElement } onLoad={ () =>  setIframeSize( iFrameElement.current ) } className="slider-image" src={ url.value } key={ guid() }></img> }) }
+            { youtubeDemo() }
           </Slider>
         </div>
       </div>
       )
     } else {
       return (
-
+        // Little Screen
         <div className="project-columns">
           <div className="slider-images">
             <Slider {...sliderSettings}>
-              { urls.map( ( url ) => { return <img ref={ imgElement } src={ url.value } key={ guid() }></img> }) }
+              { urls.map( ( url ) => { return <img ref={ imgElement } className="slider-image" src={ url.value } key={ guid() }></img> }) }
+              { youtubeDemo() }
             </Slider>
           </div>
-          <div className="project-row ">
+          <div className="project-row " onLoad={ () =>  setIframeSize(iFrameElement.current) }>
             <p className="project-text" style={{ color: "#fff" }}>
-              { writeUpText.map( ( word ) => { return keyWords.includes(word)? <strong key={ guid() } >{ `${word} ` }</strong> : `${word} ` })}
+              { writeUpText.map( ( word ) => { return keyWords.includes(word)? <strong key={ guid() } >{ `${ word } ` }</strong> : `${ word } ` })}
             </p>
             <section style={{ margin: "0 0 3em 0" }}>
               { githubButton() }
               { urlButton() }
-            </section>   
+            </section> 
+             
         </div>
       </div>
       )
     }
   }
 
-
-
-  // Mobile
   return (
-    <div ref={projectContainer} className="project" style={{ background: "rgba(66,72,94, 0.4)" }}>
-      {view()}
+    <div ref={ projectContainer } className="project" style={{ background: "rgba(66,72,94, 0.4)" }}>
+      { view() }
     </div>
   )
 }
